d96dabdfa0f240541089c446b93a55b0fcfe05fc
Emil Persson <emilper1999@gmail.com>
2019-09-24 16:18:58 +0200

    Added functionality to make blog posts and add them to the database

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
 
 const db = require('./db')
 
 const app = express()
+
+//Used for the body parser, used when handling forms
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
 
 app.use(express.static("public"))
 
@@ -16,10 +22,20 @@ app.get('/', function(request, response){
 })
 
 app.get('/portfolio', function(request, response){
-  const model = {
-    projects: dummyData.projects
-  }
-  response.render("portfolio.hbs", model)
+  db.getProjects(function(error, projects){
+    if(error){
+      const model = {
+        somethingWentWrong: true
+      }
+      response.render("portfolio.hbs", model)
+    }else{
+      const model = {
+        somethingWentWrong: false,
+        projects
+      }
+      response.render("portfolio.hbs", model)
+    }
+  })
 })
 
 app.get('/about', function(request, response){
@@ -56,7 +72,45 @@ app.get('/login', function(request, response){
 })
 
 app.get('/create-post', function(request, response){
-  response.render('create-post.hbs')
+  const model = {
+    validationErrors: []
+  }
+  response.render('create-post.hbs', model)
+})
+
+app.post('/create-post', function(request, response){
+  let date = new Date()
+  const postHeader = request.body.blogpostHeader
+  const postText = request.body.blogpostText
+  const postDate = date.toDateString()
+
+  const validationErrors = []
+
+  if(postHeader == ""){
+    validationErrors.push("Must enter a Header for the post")
+  }
+
+  if(postText == ""){
+    validationErrors.push("Mush enter a post body text")
+  }
+
+  if(validationErrors.length == 0){
+    db.createNewBlogPost(postHeader, postText, postDate, function(error){
+      if(error){
+        console.log("Internal server error...")
+      }else{
+        //fix this so that the server redirects the user to the post site
+        response.redirect('/blog')
+      }
+    })
+  }else{
+    const model = {
+      validationErrors,
+      postHeader,
+      postText
+    }
+    response.render("create-post.hbs", model)
+  }
 })
 
 app.listen(8080, () =>{
--- db.js
+++ db.js
@@ -1,6 +1,13 @@
 const sqlite = require('sqlite3')
 const db = new sqlite.Database("database.db")
 
+exports.getProjects = function(callback){
+    const query = "SELECT * FROM projects"
+    db.all(query, function(error, projects){
+        callback(error, projects)
+    })
+}
+
 exports.getAllBlogPosts = function(callback){
     const query = "SELECT * FROM blogpost"
 
@@ -10,11 +17,12 @@ exports.getAllBlogPosts = function(callback){
 }
 
 
-exports.createNewBlogPost = function(callback){
-    const query = "INSERT INTO blogpost(blogpostHeader, blogpostText, blogpostDate) VALUES ?"
-    const values = [blogpostHeader, blogpostText, blogpostDate]
+exports.createNewBlogPost = function(postHeader, postText, postDate, callback){
+    const query = "INSERT INTO blogpost (blogpostHeader, blogpostText, blogpostDate) VALUES (?, ?, ?)"
+    const values = [postHeader, postText, postDate]
 
     db.run(query, values, function(error){
+        const id = this.lastID
         callback(error, id)
     })
 }

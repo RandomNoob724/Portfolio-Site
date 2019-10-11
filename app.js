86f855c01e2a23890f0313995e9a115080b0c91c
Emil Persson <emilper1999@gmail.com>
2019-10-11 19:14:47 +0200

    Added hashed password and login with bcrypt

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const SQLiteStore = require('connect-sqlite3')(expressSession)
 const bcrypt = require('bcrypt') //Make this work as soon as possible
+
 //routers
 const blogRouter = require('./blogRouter')
 const portfolioRouter = require('./portfolioRouter')
@@ -15,12 +16,11 @@ const db = require('./db')
 
 const app = express()
 
-const username = "RandomAdmin"
-const password = "admin123"
+const saltRounds = 10
 
-/*
-* Make sure to make the cookies work together with the sessions
-*/
+const username = "RandomAdmin"
+
+const hash = "$2b$10$b1tuiPuE98ROC7Bj4je6fOZOIO1Vehhe5mPoK1KWfGNkuaUGb./86"
 
 //Used for the body parser, used when handling forms
 app.use(bodyParser.urlencoded({
@@ -38,7 +38,7 @@ app.use(expressSession({
   store: new SQLiteStore()
 }))
 
-app.use(function(request, response, next){
+app.use(function (request, response, next) {
   response.locals.isLoggedIn = request.session.isLoggedIn
   next()
 })
@@ -54,67 +54,80 @@ app.engine("hbs", expressHandlebars({
   defaultLayout: 'main.hbs'
 }))
 
-app.get('/', function(request, response){
+app.get('/', function (request, response) {
   response.render("home.hbs")
 })
 
-app.get('/about', function(request, response){
+app.get('/about', function (request, response) {
   response.render("about.hbs")
 })
 
-app.get('/contact', function(request, response){
+app.get('/contact', function (request, response) {
   response.render('contact.hbs')
 })
 
-app.get('/admin', function(request, response){
-  if(request.session.isLoggedIn){
+app.get('/admin', function (request, response) {
+  if (request.session.isLoggedIn) {
     response.render('admin.hbs')
-  }else{
+  } else {
     console.log("Visitor trying to access the admin site without valid login")
     response.redirect('/')
   }
 })
 
-app.get('/login', function(request, response){
+app.get('/login', function (request, response) {
   response.render('login.hbs')
 })
 
-app.post('/login', function(request, response){
+app.post('/login', function (request, response) {
   const inputedUsername = request.body.username
   const inputedPassword = request.body.password
   const validationErrors = []
 
-  if(inputedUsername != username){
-    validationErrors.push("Username does not match existing username")
-  }
-  if(inputedPassword != password){
-    validationErrors.push("Wrong password")
-  }
-  if(request.body.username == username && request.body.password == password){
-    const model = {
-      somethingWentWrong: false
+  bcrypt.compare(inputedPassword, hash, function (error, passwordMatch) {
+    if (error) {
+      const model = {
+        somethingWentWrong: true,
+        username: inputedUsername
+      }
+      response.render("login.hbs", model)
     }
-    request.session.isLoggedIn = true
-    response.redirect("/")
-  } else {
-    const model = {
-      somethingWentWrong: true,
-      validationErrors,
-      username: request.body.username
+    
+    // Check if input is correct or not and push validationErrors to the validation error array
+    if (inputedUsername != username) {
+      validationErrors.push("Username does not match existing username")
     }
-    response.render("login.hbs", model)
-  }
+
+    if (passwordMatch == false) {
+      validationErrors.push("Wrong password")
+    }
+
+    if (inputedUsername == username && passwordMatch) {
+      const model = {
+        somethingWentWrong: false
+      }
+      request.session.isLoggedIn = true
+      response.redirect("/")
+    } else {
+      const model = {
+        somethingWentWrong: true,
+        validationErrors,
+        username: inputedUsername
+      }
+      response.render("login.hbs", model)
+    }
+  })
 })
 
-app.get('/logout', function(request, response){
+app.get('/logout', function (request, response) {
   request.session.isLoggedIn = false
   response.redirect("/")
 })
 
-app.listen(8080, () =>{
+app.listen(8080, () => {
   console.log("server is starting on port", 8080)
 })
 
-app.use(function(request, response, next){
+app.use(function (request, response, next) {
   response.status(404).send("Error 404. Sorry can't find that!")
 })
